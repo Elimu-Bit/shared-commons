57b891e9e601393ee3426f6c9830c1d7bfb99052
vicky <dogovicky05@gmail.com>
2025-09-22 15:19:36 +0300

    Initial Commit

---
+++ src/main/java/com/skillforge/Shared_Commons/SharedCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.skillforge.Shared_Commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedCommonsApplication.class, args);
+	}
+
+}
