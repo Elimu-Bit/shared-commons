41ba272af021672a7277669e7f3ef0c06a920632
vicky <dogovicky05@gmail.com>
2025-09-26 09:14:42 +0300

    Revert "Finalized shared commons including utils, exceptions and response class"
    This reverts commit b9a1eb372781acad2ab37fbd871c342bc54aab75.

--- src/main/java/com/skillforge/Shared_Commons/response/ApiResponse.java
+++ src/main/java/com/skillforge/Shared_Commons/response/ApiResponse.java
@@ -1,7 +1,6 @@
 package com.skillforge.Shared_Commons.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,7 +14,6 @@ import java.util.Map;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
 
     //Core response fields
